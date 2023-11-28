81efab37dec1130a935afa72c180a7e5fd38dcd6
FridIsar <isarfridriksson@gmail.com>
2023-11-28 15:41:07 +0100

    creator archive

--- course_creator.py
+++ course_creator.py
@@ -1,78 +1,51 @@
-from bs4 import BeautifulSoup
+import csv
 from datetime import datetime, timedelta
-from icalendar import Calendar, Event
 
-import re
+# Function to parse date from the CSV file
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%d/%m/%Y %H:%M')
 
-def extract_minutes(duration_str):
-    # Regular expression to find hours and minutes
-    pattern = re.compile(r'(?:(\d+) h )?(\d+) min|\d+ min')
+# Function to check if a given date is a weekend (Saturday or Sunday)
+def is_weekend(date):
+    return date.weekday() in [5, 6]
 
-    match = pattern.search(duration_str)
-    if match:
-        hours, minutes = match.groups()
-        hours = int(hours) if hours else 0
-        minutes = int(minutes) if minutes else 0
-        total_minutes = hours * 60 + minutes
-        return total_minutes
-    else:
-        return None
+# Function to add a day to a given date, skipping weekends
+def add_weekday(date, days):
+    new_date = date + timedelta(days)
+    while is_weekend(new_date):
+        new_date += timedelta(1)
+    return new_date
 
-# Read HTML content from a file
-with open('ext.html', 'r', encoding='utf-8') as file:
-    html_content = file.read()
+# Open the CSV file
+with open('data.csv', 'r') as csvfile:
+    # Create a CSV reader
+    csvreader = csv.reader(csvfile)
 
-soup = BeautifulSoup(html_content, 'html.parser')
+    # Skip the header row
+    next(csvreader)
 
-# Extract content from spans with class "section--section-title--wcp90"
-title_spans = soup.find_all('span', 
-class_='section--section-title--wcp90')
-title_content = [span.text for span in title_spans]
+    start_date = parse_date('15/01/2024 18:00')
+    # Iterate through each row in the CSV file
+    for row in csvreader:
+        title = row[5]  # Assuming the title is in the 5 column
+        duration = float(row[4])  # Assuming the duration is in the 4 column
 
-# Extract content from spans with class "ud-text-sm 
-# section--hidden-on-mobile--38HTe section--section-content--jXmra"
-content_spans = soup.find_all('span', class_='ud-text-sm section--hidden-on-mobile--38HTe section--section-content--jXmra')
-content_content = [span.text for span in content_spans]
+        # Calculate the end date based on the duration
+        end_date = start_date + timedelta(minutes=duration)
 
-# Extract minutes from each duration string
-duration_minutes = [extract_minutes(duration) for duration in content_content]
+        # Check if the event ends before 21:30
+        if end_date.time() < datetime.strptime('21:30', '%H:%M').time():
+            event_end_date = end_date
+        else:
+            # Add it the following day at 18:00
+            event_end_date = add_weekday(start_date, 1)
+            event_end_date = event_end_date.replace(hour=18, minute=0)
 
-event_data = dict(zip(title_content, duration_minutes))
+        # Skip weekends
+        while is_weekend(event_end_date):
+            event_end_date = add_weekday(event_end_date, 1)
+            
 
-initial_start_time = datetime.now().replace(hour=15, minute=0, second=0, microsecond=0)
 
-# Create a new calendar
-cal = Calendar()
-
-# Initialize the start time
-start_time = initial_start_time
-
-# Iterate through the dictionary and create events
-for event_name, duration_minutes in event_data.items():
-    # Create an event
-    event = Event()
-
-    # Set the event summary (name)
-    event.add('summary', event_name)
-
-    # Set the event start and end times
-    end_time = start_time + timedelta(minutes=duration_minutes)
-    event.add('dtstart', start_time)
-    event.add('dtend', end_time)
-
-    # Add the event to the calendar
-    cal.add_component(event)
-
-    # Update the start time for the next event
-    if duration_minutes < 50 and end_time.time() < datetime.strptime('16:40', '%H:%M').time():
-        # If the duration is short and the end time is before 7:40 pm, start the next event on the same day
-        start_time = end_time
-    else:
-        # Otherwise, start the next event on the next day at 6 pm
-        start_time = (start_time + timedelta(days=1)).replace(hour=15, minute=0, second=0, microsecond=0)
-
-# Save the calendar to a file
-with open('calendar.ics', 'wb') as f:
-    f.write(cal.to_ical())
-
-print("Calendar created and saved to 'calendar.ics'")
+        print(f"Event: {title}, Start Date: {start_date}, End Date: {event_end_date}")
+        start_date = end_date
